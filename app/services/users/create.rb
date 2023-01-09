ee7d75b58689b4b87d1f75b92858d2c21845db67
Mateusz Bialowas <mateusz.bialowas2@gmail.com>
2023-01-09 23:28:36 +0100

    Implement service object with dry-monads do notation.

--- app/services/users/create.rb
+++ app/services/users/create.rb
@@ -7,7 +7,39 @@ module Users
     end
 
     def call
-      # TODO: write your code here. You can inspire by this code: https://github.com/mateuszbialowas/brug-service_objects/pull/1/files
+      user_params = yield validate_user_params
+      shop_params = yield validate_shop_params
+      ActiveRecord::Base.transaction do
+        shop = yield create_shop(shop_params)
+        user = yield create_user(shop, user_params)
+
+        # send email
+        RegistrationMailer.with(user:).welcome_email.deliver_later
+
+        Success({ user:, shop:, message: 'User registered successfully' })
+      end
+    end
+
+    private
+
+    def validate_user_params
+      result = CreateUserValidator.new.call(@user_params)
+      result.success? ? Success(result) : Failure(result.errors.to_h)
+    end
+
+    def validate_shop_params
+      result = CreateShopValidator.new.call(@shop_params)
+      result.success? ? Success(result) : Failure(result.errors.to_h)
+    end
+
+    def create_shop(shop_params)
+      shop = Shop.new(industry_id: shop_params[:industry_id], name: shop_params[:name])
+      shop.save ? Success(shop) : Failure(shop.errors.full_messages.join(', '))
+    end
+
+    def create_user(shop, user_params)
+      user = User.new(shop:, name: user_params[:name], email: user_params[:email])
+      user.save ? Success(user) : Failure(user.errors.full_messages.join(', '))
     end
   end
 end
--- spec/services/users/create_spec.rb
+++ spec/services/users/create_spec.rb
@@ -2,5 +2,64 @@
 require 'rails_helper'
 
 RSpec.describe Users::Create, type: :service do
-  # TODO: write your tests here. You can inspire by this code: https://github.com/mateuszbialowas/brug-service_objects/pull/1/files
+  subject(:service) { described_class.new(user_params:, shop_params:).call }
+  let(:industry) { create(:industry) }
+  let(:industry_id) { industry.id }
+  let(:user_params) { { name: 'John', email: 'john2n@gmail.com' } }
+  let(:shop_params) { { industry_id:, name: 'John Shop' } }
+
+  context 'when shop_params are invalid' do
+    let(:industry_id) { 0 }
+
+    it 'returns failure' do
+      expect { service }.to not_change { User.count }.and not_change { Shop.count }
+      expect(service.failure).to eq(industry_id: ['Industry not found'])
+    end
+  end
+
+  context 'when user params are invalid' do
+    let(:user_params) { { name: nil, email: nil } }
+
+    it 'returns failure' do
+      expect { service }.to not_change { User.count }.and not_change { Shop.count }
+      expect(service.failure).to eq({ name: ['must be filled'], email: ['must be filled'] })
+    end
+  end
+
+  context 'when error creating shop' do
+    before do
+      allow_any_instance_of(Shop).to receive(:save).and_return(false)
+      allow_any_instance_of(Shop).to receive(:errors).and_return(double(full_messages: ['Shop error']))
+    end
+
+    it 'returns failure' do
+      expect { service }.to not_change { User.count }.and not_change { Shop.count }
+      expect(service.failure).to eq('Shop error')
+    end
+  end
+
+  context 'when error creating user' do
+    let(:user_double) { instance_double(User, save: false, errors: double(full_messages: ['User error'])) }
+
+    before do
+      allow(User).to receive(:new).and_return(user_double)
+    end
+
+    it 'returns failure' do
+      expect { service }.to not_change { User.count }.and not_change { Shop.count }
+      expect(service.failure).to eq('User error')
+    end
+  end
+
+  context 'when params are valid and success creating user and shop' do
+    it 'returns success' do
+      expect { service }.to change { User.count }.by(1).and change { Shop.count }.by(1)
+      expect(service.success[:user]).to be_a(User)
+      expect(service.success[:shop]).to be_a(Shop)
+      expect(service.success[:message]).to eq('User registered successfully')
+    end
+    it 'sends registration email' do
+      expect { service }.to have_enqueued_mail(RegistrationMailer, :welcome_email)
+    end
+  end
 end
