1eca9c02d7a323a6a23689c5c6115f9393cb6570
Adam Piotrowski <adamsarin@gmail.com>
2023-01-10 13:53:39 +0100

    In order to present add user && shop create feature, I added PORO base service with simplest methods to handle errors and result. In User Create service we handled creating shop and user flow, that ends with sending email. There is error catching in case of any unexpected error.

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -2,6 +2,38 @@
 
 require 'dry/monads'
 
-class BaseService
-  include Dry::Monads[:do, :result]
+module BaseService
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
+  module ClassMethods
+    def call(**args)
+      new(**args).tap(&:call)
+    end
+  end
+  def result
+    @_result ||= {}
+  end
+
+  def errors
+    @_errors ||= []
+  end
+
+  def success?
+    errors.blank?
+  end
+
+  def failure?
+    !success?
+  end
+
+  protected
+
+  def add_error(error)
+    errors.push(error)
+  end
+
+  def add_result(**hash)
+    result.merge!(**hash)
+  end
 end
